d9da33e883a3de9eb84eb10307ace64f51b9fb90
shaxruxpirimbetov <shaxruxpirimbetov@gmail.com>
2025-11-18 14:09:35 +0500

    WebPageBot

---
+++ handlers.py
@@ -0,0 +1,9 @@
+from aiogram import F, Router
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+router = Router()
+
+@router.message(CommandStart)
+async def cmd_start(message: Message):
+    await message.answer("Hi")
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from aiogram import Bot, Dispatcher
 import config as conf
-import asyncio
+import asyncio, handlers
 
 bot = Bot(token=conf.TOKEN)
 dp = Dispatcher()
-
+dp.include_router(handlers.router)
 
 async def main():
+    await bot.send_message(chat_id=6181120570, text="I`m here")
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        print(f"Done with {e}")
